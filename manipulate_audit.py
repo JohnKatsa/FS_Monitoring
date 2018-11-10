282fbd5597d68941ee47af3898c1407cffcf770b
JohnKatsa <johnkatsa66@gmail.com>
2018-11-10 18:33:31 +0200

    add time

--- manipulate_audit.py
+++ manipulate_audit.py
@@ -1,6 +1,6 @@
 
 """function to determine read/write arguments"""
-def read_write_args(data,sys_id,pid_to_fhs,fh_to_fn_and_p):
+def read_write_args(data,sys_id,fh_to_fn_and_p,time):
     for token in data:
         if "pid=" in token:
             pid = token.split("=")[1]  # take second arg (collision with ppid, but pid is second so it keeps correct value)
@@ -9,27 +9,23 @@ def read_write_args(data,sys_id,pid_to_fhs,fh_to_fn_and_p):
         elif "exit=" in token:
             bytes_num = int(token.split("=")[1])
 
-    pos = 0
+    name = ""
     # write the results to map
     for x in fh_to_fn_and_p:
         # found record in map
-        if x[0] == fh:
-            pos = x[2]
-            x[2] += bytes_num
+        if x[0] == pid and x[1] == fh:
+            pos = x[3]
+            x[3] += bytes_num
+            name = x[2]
 
-    # find name
-    name = ""
-    for x in fh_to_fn_and_p:
-        if x[0] == fh:
-            name = x[1]
+
     if sys_id == 0 and name: # read
-        print "READ\t", name, "\t", pos, "\t", bytes_num
+        print time, "\t", "READ\t", name, "\t", pos, "\t", bytes_num, pid
     elif sys_id == 1 and name:   # write
-        print "WRITE\t", name, "\t", pos, "\t", bytes_num
-
+        print time, "\t", "WRITE\t", name, "\t", pos, "\t", bytes_num, pid
 
 """function to determine open arguments"""
-def open_args(data,pid_to_fhs,fh_to_fn_and_p):
+def open_args(data,fh_to_fn_and_p,time):
     for token in data:
         if "pid=" in token:
             pid = token.split("=")[1]  # take second arg (collision with ppid, but pid is second so it keeps correct value)
@@ -38,23 +34,16 @@ def open_args(data,pid_to_fhs,fh_to_fn_and_p):
         elif "exit=" in token:
             fh = token.split("=")[1]
 
-    #search for pid
-    flag = 0
-    for p in pid_to_fhs:
-        #found it
-        if p[0] == pid:
-            flag = 1
-            p[1].append(fh)
-    if flag == 0:   # first time this pid occured
-        pid_to_fhs.append([pid,[fh]])
+    # check if same pid with same fh does it more than once
+    for x in fh_to_fn_and_p:
+        if x[0] == pid and x[1] == fh:
+            return
+    fh_to_fn_and_p.append([pid,fh,name,0])
 
-    # store file handler
-    fh_to_fn_and_p.append([fh,name,0])
-
-    print "OPEN\t", name
+    print time, "\t", "OPEN\t", name, pid, fh
 
 """function to determine open arguments"""
-def close_args(data,pid_to_fhs,fh_to_fn_and_p):
+def close_args(data,fh_to_fn_and_p,time):
     for token in data:
         if "pid=" in token:
             pid = token.split("=")[1]  # take second arg (collision with ppid, but pid is second so it keeps correct value)
@@ -62,37 +51,53 @@ def close_args(data,pid_to_fhs,fh_to_fn_and_p):
             fh = token.split("=")[1]
 
     #search for pid
-    for p in pid_to_fhs:
+    for p in fh_to_fn_and_p:
         #found it
-        if p[0] == pid:
-            if len(p[1]) == 1:  # last item
-                del p
-            else:
-                del p[1]
+        if p[0] == pid and p[1] == fh:
+            print time, "\t", "CLOSE\t", p[2], pid
+            fh_to_fn_and_p.remove(p)
 
-    # store file handler
+"""function to determine lseek arguments"""
+def lseek_args(data,fh_to_fn_and_p,time):
+    for token in data:
+        if "pid=" in token:
+            pid = token.split("=")[1]  # take second arg (collision with ppid, but pid is second so it keeps correct value)
+        elif "a0=" in token:
+            fh = token.split("=")[1]
+        elif "a1=" in token:
+            offset = token.split("=")[1]
+
     for x in fh_to_fn_and_p:
-        if x[0] == fh:
-            del x
-
-    print "CLOSE\t", name
+        if x[0] == pid and x[1] == fh:
+            x[3] += offset
 
 
 """function to read an audit record and extract data"""
-def read_record(data,pid_to_fhs,fh_to_fn_and_p):
+def read_record(data,fh_to_fn_and_p):
+    timeflag = 0
+    time = ""
     for token in data:
+        if "time->" in token:
+            time += (token.split("->")[1]+' ')
+            timeflag = 4
+        elif timeflag:
+            time += (token+' ')
+            timeflag -= 1
         # read
-        if "syscall=0" in token:
-            read_write_args(data,0,pid_to_fhs,fh_to_fn_and_p)
+        elif "syscall=0" == token:
+            read_write_args(data,0,fh_to_fn_and_p,time)
         # write
-        if "syscall=1" in token:
-            read_write_args(data,1,pid_to_fhs,fh_to_fn_and_p)
+        elif "syscall=1" == token:
+            read_write_args(data,1,fh_to_fn_and_p,time)
         # open
-        if "syscall=2" in token:
-            open_args(data,pid_to_fhs,fh_to_fn_and_p)
+        elif "syscall=2" == token:
+            open_args(data,fh_to_fn_and_p,time)
         # close
-        if "syscall=3" in token:
-            close_args(data,pid_to_fhs,fh_to_fn_and_p)
+        elif "syscall=3" == token:
+            close_args(data,fh_to_fn_and_p,time)
+        # lseek
+        elif "syscall=8" == token:
+            lseek_args(data,fh_to_fn_and_p,time)
 
     #print data, " \\\\\ "
 
@@ -100,22 +105,18 @@ def main():
     useful = []
     record = ""
 
-    pid_to_fhs = []  # list with an process id and a list of fhs as an item
-    fh_to_fn_and_p = [] # list with a file handler, a file name and a file pointer
+    fh_to_fn_and_p = [] # list with a pid, a file handler, a file name and a file pointer
 
-    with open ("report.txt", "r") as myfile:
+    with open ("report2.txt", "r") as myfile:
         for line in myfile:
             if "----" not in line:
                 useful.append(line)
             else:   # new record
                 if record:  # if there has been data, store them
-                    read_record(record.split(),pid_to_fhs,fh_to_fn_and_p)
+                    read_record(record.split(),fh_to_fn_and_p)
                 useful = []
             record = "".join(useful)
-
-        print pid_to_fhs
-        print "----------"
-        print fh_to_fn_and_p
+    print fh_to_fn_and_p
 
 if __name__ == "__main__":
     main()
