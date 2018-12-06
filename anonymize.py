88bfdac03a6333c1e79999c4470000eaa6407920
JohnKatsa <johnkatsa66@gmail.com>
2018-12-06 13:04:47 +0200

    ready anonymization and deanonymization function

--- anonymize.py
+++ anonymize.py
@@ -2,6 +2,23 @@
 def anonymize(name):
     retname = ""
     for c in name:
-        retname += chr((ord(c)-17)%(126-33) + 34)
+        if ord(c) <= 33 + 31:
+            retname += chr(ord(c) + 31)
+        elif ord(c) <= 33 + 62:
+            retname += chr(ord(c) + 31)
+        else:
+            retname += chr(ord(c) - 62)
+
+    return retname
+
+def deanonymize(name):
+    retname = ""
+    for c in name:
+        if ord(c) <= 33 + 31:
+            retname += chr(ord(c) + 62)
+        elif ord(c) <= 33 + 62:
+            retname += chr(ord(c) - 31)
+        else:
+            retname += chr(ord(c) - 31)
 
     return retname
