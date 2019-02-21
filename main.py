dcb441e2d9028cdd325057f9477507259f2c844b
JohnKatsa <johnkatsa66@gmail.com>
2019-02-21 22:17:45 +0200

    added signal handler SIGINT and SIGTERM

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import platform
 import time
 import subprocess
 import getpass
+import signal
 
 def main():
 
@@ -23,12 +24,37 @@ def main():
     else:
         res = subprocess.run(['auditctl', '-a', 'always,exit', '-F', 'arch=b32', '-F', 'success=1', '-S', 'open,close,read,write,dup,dup2,fork,vfork,clone,lseek,mkdir,rmdir'], stdout=subprocess.PIPE)
     
-    # at the beginning we find file sizes
+    # at the beginning we find file sizes and delete previous checkpoint file and log
+    # immediately delete /var/log/audit/*
+    subprocess.run(['rm', '/var/log/audit/*'])
+    res = subprocess.run(['rm', 'checkfile.txt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     filesMap = {}
-    filesMap = makeFilesMap(os.environ['HOME']+'/')
-    #filesMap = makeFilesMap("/home/"+getpass.getuser()+"/")   # e.g. "/home/"
+    filesMap = makeFilesMap(os.environ['HOME']+'/')     # get user's file sizes
 
     currMap = {}
+
+    # handle SIGINT and SIGTERM
+    def signal_handler(sig, frame):
+        currMap = {}
+        print('You pressed Ctrl+C!')
+        # ausearch with file
+        result = subprocess.run(['ausearch', '--checkpoint', 'checkfile.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if result == 0:   #(stdout is null in case of error) run ausearch -ts
+            result = subprocess.run(['ausearch', '--start', 'checkpoint', 'checkfile.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            
+        file = open(filename,"a+")
+        file.write(result)
+        file.close()
+
+        # parse and upload
+        print(filename,filenameout)
+        currMap = parse(filename,filenameout,filesMap,currMap)
+
+        sys.exit(0)
+        
+    # set signal handlers
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
     # do it for ever
     i = 0
@@ -37,13 +63,14 @@ def main():
         filenameout = "./ftp/log" + str(i) + "out.log"
 
         # then we sleep until its time to parse and upload
-        time.sleep(180)
-        
+        if i != 0:
+            time.sleep(30)
+            
         # ausearch with file
-        result = subprocess.run(['ausearch', '--checkpoint', 'temp.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        if not result:   #(stdout is null in case of error) run ausearch -ts
-            result = subprocess.run(['ausearch', '--start', 'checkpoint', 'temp.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-        
+        result = subprocess.run(['ausearch', '--checkpoint', 'checkfile.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if result == 0:   #(stdout is null in case of error) run ausearch -ts
+            result = subprocess.run(['ausearch', '--start', 'checkpoint', 'checkfile.txt', '-i'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            
 
         file = open(filename,"a+")
         file.write(result)
@@ -52,12 +79,11 @@ def main():
         # parse and upload
         currMap = parse(filename,filenameout,filesMap,currMap)
 
-        # immediately delete /var/log/audit/*
-        #subprocess.run(['rm', '/var/log/audit/*'])
-        #subprocess.run(['rm', 'log.log'])
-
         # FTP log transfer 
         # https://www.pythonforbeginners.com/code-snippets-source-code/how-to-use-ftp-in-python
+
+        i += 1
+
 
 if __name__ == "__main__":
     main()
